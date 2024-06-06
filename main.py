0b64a09154c8f7c60787eb61c5718d65ed74832a
Simon-Joshua <158138258+Simon-Joshua@users.noreply.github.com>
2024-06-06 14:41:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -70,3 +70,23 @@ Class ChildrensAccount(Account):
 
     def __str__(self):
         return f"ChildrensAccount({self.account_number}, Balance: {self._balance})"
+
+# Simon Joshua 
+class StudentAccount(Account):
+    def __init__(self, account_number, balance=0):
+        super().__init__(account_number, balance)
+        self.withdrawal_limit = 2000
+        self.deposit_limit = 50000
+
+    def deposit(self, amount):
+        if amount <= self.deposit_limit:
+            return super().deposit(amount)
+        return False
+
+    def withdraw(self, amount):
+        if amount <= self.withdrawal_limit:
+            return super().withdraw(amount)
+        return False
+
+    def __str__(self):
+        return f"StudentAccount({self.account_number}, Balance: {self._balance})"
